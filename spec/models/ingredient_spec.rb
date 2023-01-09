e96ad6227ad1ca2c914fa7274599e6ee8bb92d4c
Anthony Ongaro <43623494+ajongaro@users.noreply.github.com>
2023-01-08 21:44:27 -0700

    Double check relationships

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -3,5 +3,6 @@ require 'rails_helper'
 RSpec.describe Ingredient, type: :model do
   describe 'relationships' do
     it { should have_many :dishes }
+    it { should have_many(:chefs).through(:dishes)}
   end
 end
